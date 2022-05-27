b60e8d5e6e921edcca12c8dd02da332f144de0d8
0AMIN0 <96407363+0AMIN0@users.noreply.github.com>
2022-05-27 12:26:16 -0700

    In this row, we made and defined the Harrow button In this row, we defined the 1, 2, 3 buttons in the same way, giving each one a digit class, then giving minus and dividing two different methods.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,5 +13,12 @@
            </td>
 
         <td>
-        </td>
-      </tr>
+      </tr>        </td>
+ 
+        <tr>
+         <td><button @click="digit(1)" class="digit">1</button></td>
+         <td><button @click="digit(2)" class="digit">2</button></td>
+         <td><button @click="digit(3)" class="digit">3</button></td>
+         <td><button @click="minus" id="minus">-</button></td>
+         <td><button @click="mod" id="mod">%</button></td>
+       </tr>
