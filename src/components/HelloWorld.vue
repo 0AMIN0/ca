4602f9d32eb7b65fab39c82137f5fca27b2d7eef
0AMIN0 <96407363+0AMIN0@users.noreply.github.com>
2022-05-27 13:02:34 -0700

    we opened the script and we select the 5 value

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -45,3 +45,18 @@
            </table>
   </div>
 </template>
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      screen: "",
+      display: "",
+      value1: Number,
+      value2: Number,
+      op: "",
+    };
+  },
