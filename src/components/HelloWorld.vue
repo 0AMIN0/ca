2dd713dacbf4bacf22e5f75bbdad9dce93af64a9
0AMIN0 <96407363+0AMIN0@users.noreply.github.com>
2022-05-27 13:11:03 -0700

    add methods any numbers in methods will give plus 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
- <div>
+  <div>
     <table id="calculator" dir="ltr" style="text-align: center; margin: auto">
       <tr>
         <td colspan="5">
@@ -10,39 +10,38 @@
             type="number"
             style="padding: 5px; font-weight: bold; text-align: right"
           />
-           </td>
+        </td>
 
-        <td>
-            </td>
-      </tr>       
-        <tr>
-         <td><button @click="digit(1)" class="digit">1</button></td>
-         <td><button @click="digit(2)" class="digit">2</button></td>
-         <td><button @click="digit(3)" class="digit">3</button></td>
-         <td><button @click="minus" id="minus">-</button></td>
-         <td><button @click="mod" id="mod">%</button></td>
-       </tr>
-       <tr>
+        <td></td>
+      </tr>
+      <tr>
+        <td><button @click="digit(1)" class="digit">1</button></td>
+        <td><button @click="digit(2)" class="digit">2</button></td>
+        <td><button @click="digit(3)" class="digit">3</button></td>
+        <td><button @click="minus" id="minus">-</button></td>
+        <td><button @click="mod" id="mod">%</button></td>
+      </tr>
+      <tr>
         <td><button @click="digit(4)" class="digit">4</button></td>
         <td><button @click="digit(5)" class="digit">5</button></td>
         <td><button @click="digit(6)" class="digit">6</button></td>
         <td><button @click="mul" id="mul">*</button></td>
         <td><button @click="remove" id="remove">del</button></td>
-       </tr>
-        <tr>
+      </tr>
+      <tr>
         <td><button @click="digit(7)" class="digit">7</button></td>
         <td><button @click="digit(8)" class="digit">8</button></td>
         <td><button @click="digit(9)" class="digit">9</button></td>
         <td><button @click="div" id="div">/</button></td>
         <td><button @click="clear" id="clear">C</button></td>
-       </tr>
-        <tr>
+      </tr>
+      <tr>
         <td colspan="2"><button @click="digit(0)" class="digit">0</button></td>
         <td><button @click="sum" id="sum">+</button></td>
         <td><button @click="decimal" id="decimal">.</button></td>
         <td><button @click="equal" id="equal">=</button></td>
-       </tr>
-           </table>
+      </tr>
+    </table>
   </div>
 </template>
 <script>
@@ -60,3 +59,7 @@ export default {
       op: "",
     };
   },
+  methods: {
+    digit(n) {
+      this.screen += n;
+    },
